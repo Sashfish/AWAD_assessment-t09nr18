a6796ab5da7f89042da334ac94bc63fd7538d7a0
Sashfish <dormiii4@gmail.com>
2019-12-07 16:43:19 +0000

    leaflet working

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,5 @@
+class MapsController < ApplicationController
+def index
+@properties = Property.all
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get '/weather'=>'livedata#index'
+  get '/map'=>'maps#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
