78bbf94f31326cdf8e2d1c129de097a31585161b
Sashfish <dormiii4@gmail.com>
2019-12-09 17:21:43 +0000

    fleshed out charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,8 @@
 class ChartsController < ApplicationController
   def index
-    @properties = Property.all
+    prices1 = Property.where(price: 0..300000).count
+    prices2 = Property.where(price: 300000..700000).count
+    prices3 = Property.where('price > 700000').count
+    @p = {'Under $300000'=>prices1, '$300000 - $700000'=>prices2, '$700000+'=>prices3}
   end
 end
